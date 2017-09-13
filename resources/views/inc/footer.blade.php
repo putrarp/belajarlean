d84414643d5618f59c368511ea59d03c8384548b
Putra Rizki Pradana <putrarpradana@gmail.com>
2017-09-13 16:24:30 +0700

    Edit Footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="blog-footer">
     <p class="pull-right"><a href="#">Back to top</a></p>
-    <p>&middot; &copy; 2017 belajarlean.com &middot;</p>
+    <p>&copy; 2017 belajarlean.com</p>
 </footer>
