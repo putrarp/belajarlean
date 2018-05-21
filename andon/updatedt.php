1d4eb7942975b63c42c08756870a5aa166ec3549
putrarp <putrarpradana@gmail.com>
2018-05-21 09:15:55 +0700

    Date Change

--- andon/updatedt.php
+++ andon/updatedt.php
@@ -7,12 +7,12 @@
     $status = $_GET["status"];
     $laststatus = $_SESSION["laststatus"];
     $start = $_SESSION["lastupdate"];
-    $end = new DateTime(date("Y-m-d h:i:s"));
-    $end->setTimezone(new DateTimeZone("Asia/Jakarta"));
-    $sql = "UPDATE `machine` SET `machineStatus`= $status ,`lastUpdate`= NOW() WHERE `ID` = $id";
+    $now = new DateTime(NOW());
+    $now->setTimezone(new DateTimeZone("Asia/Jakarta"));
+    $sql = "UPDATE `machine` SET `machineStatus`= $status ,`lastUpdate`= $now WHERE `ID` = $id";
     $conn->query($sql);
     if ($laststatus <> 1){
-        $sql = "INSERT INTO `record` (`ID`, `machineID`, `type`, `start`, `end`) VALUES (NULL, '$MC', '$laststatus', '$start', NOW())";
+        $sql = "INSERT INTO `record` (`ID`, `machineID`, `type`, `start`, `end`) VALUES (NULL, '$MC', '$laststatus', '$start', '$now')";
         $conn->query($sql);
     }
     header("Location: ./apps.php"); 
