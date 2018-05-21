85a0b6ec73aebaf0e5d37b8330353037597dec99
putrarp <putrarpradana@gmail.com>
2018-05-21 09:08:49 +0700

    Edit Timezone

--- andon/updatedt.php
+++ andon/updatedt.php
@@ -9,6 +9,7 @@
     $laststatus = $_SESSION["laststatus"];
     $start = $_SESSION["lastupdate"];
     $end = new DateTime(date("Y-m-d h:i:s"));
+    $end->setTimezone(new DateTimeZone("Asia/Jakarta"));
     $sql = "UPDATE `machine` SET `machineStatus`= $status ,`lastUpdate`= NOW() WHERE `ID` = $id";
     $conn->query($sql);
     if ($laststatus <> 1){
